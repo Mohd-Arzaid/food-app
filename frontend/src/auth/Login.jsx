5a24548499e44766fb6a0b67567b81895e080fec
Mohd-Arzaid <arzaid0101030@gmail.com>
2025-01-23 00:40:51 +0530

    frontend login page ui

---
+++ frontend/src/auth/Login.jsx
@@ -0,0 +1,77 @@
+import { Link } from "react-router-dom";
+import { Eye, EyeOff } from "lucide-react";
+import { useState } from "react";
+
+const Login = () => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const togglePassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  return (
+    <div className="flex min-h-screen justify-center items-center">
+      <div className="border-2 border-black/10 shadow-lg shadow-black/10 w-full max-w-md m-4 md:m-auto p-4 rounded-lg">
+        <h1 className="font-semibold text-2xl text-center mb-5">
+          Welcome Back
+        </h1>
+
+        <form className="flex flex-col gap-2">
+          {/* Email */}
+          <label className="flex flex-col gap-2">
+            Email
+            <input
+              type="email"
+              placeholder="Enter your email address"
+              className="bg-transparent fill-none border-2 border-black/20 duration-200 focus:border-green-700 text-black p-2 focus:outline-none rounded-lg"
+            />
+          </label>
+
+          {/* Password */}
+          <label className="flex flex-col gap-2">
+            Password
+            <div className="relative">
+              <input
+                type={showPassword ? "text" : "password"}
+                placeholder="Enter your password"
+                className="bg-transparent fill-none border-2 border-black/20 duration-200 focus:border-green-700 text-black p-2 focus:outline-none rounded-lg w-full"
+              />
+              <button
+                type="button"
+                onClick={togglePassword}
+                className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-500 hover:text-gray-700"
+              >
+                {showPassword ? (
+                  <EyeOff size={20} />
+                ) : (
+                  <Eye size={20} />
+                )}
+              </button>
+            </div>
+          </label>
+
+          <div className="flex justify-start mt-3">
+            <Link to="/forgot-password" className="text-green-700">
+              Forgot Password?
+            </Link>
+          </div>
+
+          <button
+            className="p-3 bg-green-600 text-white cursor-pointer rounded-lg mt-3 font-semibold duration-200"
+          >
+            Login
+          </button>
+
+          <span className="text-center mt-2">
+            Don&apos;t have an account?{" "}
+            <Link to="/register" className="text-green-600">
+              Signup
+            </Link>
+          </span>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
