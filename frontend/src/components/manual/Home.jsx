ce087c6b589f34468ddae2a3a97ec9a189b63be6
Mohd-Arzaid <arzaid0101030@gmail.com>
2025-01-23 10:34:12 +0530

    for testing

--- frontend/src/components/manual/Home.jsx
+++ frontend/src/components/manual/Home.jsx
@@ -1,17 +1,30 @@
-
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
     <div>
-      <Link to="/login">
-      Login
-      </Link>
-      <Link to="/signup">   
-      Sign Up
-      </Link>
+      <h1>Welcome to Home Page</h1>
+      <ul>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+        <li>
+          <Link to="/signup">Sign Up</Link>
+        </li>
+        <li>
+          <Link to="/forgot-password">Forgot Password</Link>
+        </li>
+        <li>
+          <Link to="/update-password/sample-token">
+            Update Password (Token Example)
+          </Link>
+        </li>
+        <li>
+          <Link to="/verify-email">Verify Email</Link>
+        </li>
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
